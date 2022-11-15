f34d2f0fed2141039b9ea02d697b46ce05268f54
v3ry <v3ryfr@gmail.com>
2022-11-15 10:49:09 +0100

    first commit

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,13 +1,6 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import java.sql.Date;
 
